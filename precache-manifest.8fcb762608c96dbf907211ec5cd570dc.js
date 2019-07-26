ad36d52d886c3112cd559b25d8c553dbb6076ed1
javieramontenegro <doki.snsd@gmail.com>
2019-07-26 17:37:49 -0400

    Updates

---
+++ precache-manifest.8fcb762608c96dbf907211ec5cd570dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c24bfaea69227cdffb28b89212fa755f",
+    "url": "/static/media/logo.c24bfaea.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23a67e7458088e80e44d",
+    "url": "/static/js/main.ba16061c.chunk.js"
+  },
+  {
+    "revision": "ebb7cdc435b08799fabc",
+    "url": "/static/js/2.e5ebbfbc.chunk.js"
+  },
+  {
+    "revision": "23a67e7458088e80e44d",
+    "url": "/static/css/main.7aa39c89.chunk.css"
+  },
+  {
+    "revision": "179a9a1bd62fcabceabdbcb5004f2054",
+    "url": "/index.html"
+  }
+];
